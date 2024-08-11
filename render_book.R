5ffafe46e2a093cfcf5333bf605de591346d296b
jonasfreimuth <68993915+jonasfreimuth@users.noreply.github.com>
2024-08-11 14:12:37 +0200

    render_book: Add inactive pdf infrastructure

--- render_book.R
+++ render_book.R
@@ -1,18 +1,34 @@
 import::from("here", "here")
+import::from("purrr", "walk")
 
 suppressMessages(here::i_am("render_book.R"))
 
 # Merged debug file bookdown leaves after errors. I don't want to manually
 # delete it every time, it hasn't been helpulf so far.
-debug_file <- here("bookdown/_main.Rmd")
+debug_files <- c(
+  here("bookdown/_main.Rmd"),
+  here("bookdown/_main.md")
+)
 
-if (file.exists(debug_file)) unlink(debug_file)
+walk(
+  debug_files,
+  \(debug_file) if (file.exists(debug_file)) unlink(debug_file)
+)
 
-message(
-  bookdown::render_book(
-    here("bookdown"),
-    output_format = bookdown::html_document2(
-      toc = TRUE
+output_type <- "html"
+
+switch(
+  output_type,
+  "html" = message(
+    bookdown::render_book(
+      here("bookdown"),
+      output_format = "bookdown::html_document2"
+    )
+  ),
+  "pdf" = message(
+    bookdown::render_book(
+      here("bookdown"),
+      output_format = "bookdown::pdf_document2"
     )
   )
 )
