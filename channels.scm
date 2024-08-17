5a568ef7a4ceb7955e29d3f1fdb0d3bc3add1d81
jonasfreimuth <68993915+jonasfreimuth@users.noreply.github.com>
2024-08-17 18:33:25 +0200

    channels: Update channels
      For development pacakges via cran.

--- channels.scm
+++ channels.scm
@@ -1,17 +1,23 @@
 (list (channel
-        (name 'guix)
-        (url "https://git.savannah.gnu.org/git/guix.git")
-        (branch "master")
-        (commit
-          "a4e75305e29318f19cf7e23e21778006d3f64e30")
-        (introduction
-          (make-channel-introduction
-            "9edb3f66fd807b096b48283debdcddccfea34bad"
-            (openpgp-fingerprint
-              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
-      (channel
         (name 'guix-cran)
         (url "https://github.com/guix-science/guix-cran.git")
         (branch "master")
         (commit
-          "716f714b037e8c29637868aba394a8a81db98467")))
+          "758961950ef9b9dca6f21c3644605868ef23fb59"))
+      (channel
+        (name 'guix-bioc)
+        (url "https://github.com/guix-science/guix-bioc.git")
+        (branch "master")
+        (commit
+          "e61285cc5ef69aeedc7aa507125de5bc9ec15923"))
+      (channel
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (branch "master")
+        (commit
+          "41db573403ac0ade033dfe6af09c187ebe6506b5")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
