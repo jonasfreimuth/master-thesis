f3d446b735666de9a87967fc3f1dc0359d6ab02d
jonasfreimuth <68993915+jonasfreimuth@users.noreply.github.com>
2024-09-04 15:12:23 +0200

    render_book: Copy output to root

--- render_book.R
+++ render_book.R
@@ -5,10 +5,19 @@ suppressMessages(here::i_am("render_book.R"))
 
 import::from(
   "utils.R",
+  "copy_output",
   "render_book",
   .character_only = TRUE,
   .directory = here("modules")
 )
 
+filename <- "jonas_freimuth_master_thesis"
+
+html_outfile <- here("bookdown/_main.html")
+pdf_outfile <- here("bookdown/_book/_main.pdf")
+
 render_book(here("bookdown"), format = "html")
+copy_output(html_outfile, here(paste0(filename, ".html")))
+
 render_book(here("bookdown"), format = "pdf")
+copy_output(pdf_outfile, here(paste0(filename, ".pdf")))
