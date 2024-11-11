5ea782e4ce4602113821b8712702ebf614bdb12b
jonasfreimuth <68993915+jonasfreimuth@users.noreply.github.com>
2024-11-11 19:04:08 +0100

    channels.scm: Update Guix
      To enable setting the locale from within R.
      Don't know which commit specifically enables this though.

--- channels.scm
+++ channels.scm
@@ -1,21 +1,9 @@
 (list (channel
-        (name 'guix-cran)
-        (url "https://github.com/guix-science/guix-cran.git")
-        (branch "master")
-        (commit
-          "758961950ef9b9dca6f21c3644605868ef23fb59"))
-      (channel
-        (name 'guix-bioc)
-        (url "https://github.com/guix-science/guix-bioc.git")
-        (branch "master")
-        (commit
-          "e61285cc5ef69aeedc7aa507125de5bc9ec15923"))
-      (channel
         (name 'guix)
         (url "https://git.savannah.gnu.org/git/guix.git")
         (branch "master")
         (commit
-          "41db573403ac0ade033dfe6af09c187ebe6506b5")
+          "6e19a94d5a5abd414544cba017b66799a3674a5a")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
